6acfd6cf5263108d949489b32b9aabad346fed2d
Joyyd <59011249+Joyyd@users.noreply.github.com>
2019-12-18 09:54:46 +0100

    Retest

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -7,6 +7,22 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
         }
     }
 }
